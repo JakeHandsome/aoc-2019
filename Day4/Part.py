b60a9bf6d45ded5b59f71047516b3c7ac3ac5d82
Jake Hansen <JakeMHandsome95@gmail.com>
2019-12-22 12:33:02 -0600

    Day4part2
    would have been much easier if I didn't try to figure out a regex for a while

--- Day4/Part.py
+++ Day4/Part.py
@@ -1,4 +1,6 @@
 import os
+import re
+
 f = open("{}\\Input.txt".format(os.path.dirname(os.path.realpath(__file__))),"r")
 inputs = f.readline().split("-")
 
@@ -19,6 +21,28 @@ for x in range (int(inputs[0]),int(inputs[1])):
       stringx[2] == stringx[3] or
       stringx[3] == stringx[4] or
       stringx[4] == stringx[5] )):
-      passwords.append(x)
+      passwords.append(stringx)
 
 print("Part1: {}".format(len(passwords)))
+Part2 = list()
+# Check every password we already found
+for password in passwords:
+   # At minumum we have a 1 digit streak
+   streak = 1
+   for i in range(len(password)-1):
+      # check the current digit against the next. If it matches increment our streak
+      if (password[i] == password[i+1]): 
+         streak += 1
+      # if it doesn't match
+      else:
+         # and our current streak is 2, stop we found a good password
+         if streak == 2 :
+            break
+         # other wise reset the streak and keep going
+         else:
+            streak = 1
+   # if this password had a streak of 2, keep it
+   if streak == 2:
+      Part2.append(password)
+#answer
+print("Part2: {}".format(len(Part2)))
