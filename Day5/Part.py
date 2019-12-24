ccf371f6f65049fdde887fcb7590f92f16be2087
Jake Hansen <Jakemhansen95@gmail.com>
2019-12-24 15:27:01 -0600

    Adding some comments

--- Day5/Part.py
+++ Day5/Part.py
@@ -1,14 +1,18 @@
 import os
+
+# Based on the opcode get the size of it
 def getSize(opcode):
-   if (opcode == 1 or opcode ==2):
+   if (opcode == 1 
+       or opcode ==2):
       return 4
-   elif (opcode == 3 or opcode == 4):
+   elif (opcode == 3 
+         or opcode == 4):
       return 2
    elif (opcode == 99):
       return 1
    else:
       print ("Unknown size for opcode {}".format(opcode))
-      return 0
+      exit(1)
 
 f = open("{}{}Input.txt".format(os.path.dirname(os.path.realpath(__file__)),os.path.sep),"r")
 Program = f.read().split(",")
@@ -25,14 +29,16 @@ while (i < len(Program)):
             (Program[i] // 10000) % 10)
 
    size = getSize(opcode)
- #  print ("Instrution, ",opcode) #Debug
- #  print ("Modes ", modes) #Debug
    inputsIndex = list()
+   # Convert the input based on the memory/intermediate mode
    for j in range (1,size):
       if (modes[j-1] == 0):
+         # This input is in memory mode. Set the index to be the value of the instruction
          inputsIndex.append(Program[i + j])
       else:
+         # This input is in immediate mode. Set the index to be the index of the instruction. This will cause the program to read/write to the current memory location
          inputsIndex.append(i+j)
+   #Switch based on opcode intruction
    if (opcode == 1):
       # Input 1 + Input 2 store to Input 3
       Program[inputsIndex[2]] = Program[inputsIndex[1]] + Program[inputsIndex[0]]
