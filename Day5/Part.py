52a1909f8ddcb72deba01a630c38406962634a81
Jake Hansen <Jakemhansen95@gmail.com>
2019-12-24 15:54:29 -0600

    Day5p2
    Look like opcode will probably be used more. Cleaing up the code a bit, using enumerations. This update was pretty simple. Really which python had pointers would simplify some of the statements with all the. Program[inputIndex[X]] etc...

--- Day5/Part.py
+++ Day5/Part.py
@@ -1,14 +1,31 @@
 import os
+from enum import IntEnum
+
+class Opcode(IntEnum):
+   ADD      = 1
+   MULT     = 2
+   STDIN    = 3
+   STDOUT   = 4
+   JEQ      = 5 
+   JNQ      = 6
+   LESSTHAN = 7
+   EQUALS   = 8
+   EXIT     = 99 
 
 # Based on the opcode get the size of it
 def getSize(opcode):
-   if (opcode == 1 
-       or opcode ==2):
+   if (opcode == Opcode.ADD or  
+       opcode == Opcode.MULT or
+       opcode == Opcode.LESSTHAN or
+       opcode == Opcode.EQUALS):
       return 4
-   elif (opcode == 3 
-         or opcode == 4):
+   elif (opcode == Opcode.JEQ or
+         opcode == Opcode.JNQ):
+      return 3
+   elif (opcode == Opcode.STDIN or 
+         opcode == Opcode.STDOUT):
       return 2
-   elif (opcode == 99):
+   elif (opcode == Opcode.EXIT):
       return 1
    else:
       print ("Unknown size for opcode {}".format(opcode))
@@ -39,17 +56,35 @@ while (i < len(Program)):
          # This input is in immediate mode. Set the index to be the index of the instruction. This will cause the program to read/write to the current memory location
          inputsIndex.append(i+j)
    #Switch based on opcode intruction
-   if (opcode == 1):
+   if (opcode == Opcode.ADD):
       # Input 1 + Input 2 store to Input 3
       Program[inputsIndex[2]] = Program[inputsIndex[1]] + Program[inputsIndex[0]]
-   elif(opcode == 2):
+   elif(opcode == Opcode.MULT):
       # Input 1 * Input 2 store to Input 3 
       Program[inputsIndex[2]] = Program[inputsIndex[1]] * Program[inputsIndex[0]]
-   elif (opcode == 3):
+   elif (opcode == Opcode.STDIN):
       Program[inputsIndex[0]] = int(input("Enter Input: "))
-   elif (opcode == 4):
-      print (Program[inputsIndex[0]])      
-   elif (opcode == 99):
+   elif (opcode == Opcode.STDOUT):
+      print (Program[inputsIndex[0]])   
+   elif (opcode == Opcode.JEQ):
+      if (Program[inputsIndex[0]] != 0):
+         i = Program[inputsIndex[1]] 
+         continue 
+   elif (opcode == Opcode.JNQ):
+      if (Program[inputsIndex[0]] == 0):
+         i = Program[inputsIndex[1]]
+         continue
+   elif (opcode == Opcode.LESSTHAN):
+      if (Program[inputsIndex[0]] < Program[inputsIndex[1]]):
+         Program[inputsIndex[2]] = 1
+      else:
+         Program[inputsIndex[2]] = 0
+   elif (opcode == Opcode.EQUALS):
+      if (Program[inputsIndex[0]] == Program[inputsIndex[1]]):
+         Program[inputsIndex[2]] = 1
+      else:
+         Program[inputsIndex[2]] = 0
+   elif (opcode == Opcode.EXIT):
       break;
    else:
       print("You fucked up at {}".format(i))
