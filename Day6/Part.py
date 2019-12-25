0cda334cac413b04464a9ac7d0e05eb9d62b8071
Jake Hansen <Jakemhansen95@gmail.com>
2019-12-24 19:15:31 -0600

    Day6 part2

--- Day6/Part.py
+++ Day6/Part.py
@@ -1,14 +1,28 @@
 import os
 from anytree import Node
+from anytree.search import findall
 
 def getOrbits(node):
    if (node.parent != None):
       return 1 + getOrbits(node.parent)
    else:
       return 0
-def getListOrbitNames(node):
-
-
+def findCommonParent(a,b):
+   commonParent = a
+   while True:
+      res = findall(commonParent, filter_=lambda node: node.name == b.name)
+      if (commonParent.parent == None):
+         return None
+      elif (res):
+         return commonParent
+      commonParent = commonParent.parent
+def getDistanceFromParent(node,parent):
+   distance = 1
+   currentNode = node.parent
+   while (currentNode != parent):
+      distance += 1
+      currentNode = currentNode.parent
+   return distance
 f = open("{}{}Input.txt".format(os.path.dirname(os.path.realpath(__file__)),os.path.sep),"r")
 Input = [line.rstrip() for line in f]
 
@@ -26,3 +40,8 @@ numberOfOrbits = 0
 for n in graph.values():
    numberOfOrbits += getOrbits(n)
 print ("Part 1: {}".format(numberOfOrbits))
+
+# I could just use 'Walker' for the second part but I feel like writing my own
+commonParent = findCommonParent(graph["YOU"],graph["SAN"])
+distance = getDistanceFromParent(graph["YOU"],commonParent) + getDistanceFromParent(graph["SAN"],commonParent) - 2
+print ("Part 2: {}".format(distance))
